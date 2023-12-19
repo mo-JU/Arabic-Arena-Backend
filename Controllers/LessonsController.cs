e02be7032183205e78801b87ee09134d283044c5
mo_JU <MABDELJAWAD5@GMAIL.COM>
2023-12-19 23:07:39 +0300

    ffs

--- Controllers/LessonsController.cs
+++ Controllers/LessonsController.cs
@@ -1,9 +1,11 @@
-using Arabic_Arena.Services;
+﻿using Arabic_Arena.Services;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson.IO;
 using MongoDB.Driver;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Arabic_Arena.Controllers
 {
@@ -22,9 +24,50 @@ namespace Arabic_Arena.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Lesson>>> Get()
         {
-            var lessons = await _lessonsCollection.Find(_ => true).ToListAsync();
-            return Ok(lessons);
+            //   var lessons = await _lessonsCollection.Find(_ => true).ToListAsync();
+            // return Ok(lessons);
+            var lesson = new Lesson
+            {
+                titleArabic = "فوائد الشاي الأخضر",
+                titleEnglish = "Benefits of Green Tea",
+                level = "Beginner",
+                type = "Reading",
+                video = true,
+                videoLink = "https://edpuzzle.com/media/6553a11ba164024027006fdc",
+                videoText = "nigga", // Add full HTML content as needed
+    text = "hi", // Add full HTML content as needed
+    hasTable = true,
+    table = new List<TableItem>
+    {
+        new TableItem { arabicWord = "الشاي", transcription = "Al-Shai" },
+        new TableItem { arabicWord = "سرطان", transcription = "Saratan" }
+    },
+    hasExercises = true,
+    exercises = new List<Exercise>
+    {
+        new Exercise
+        {
+            questionArabic = "كم حرف باللغة العربية؟",
+            questionEnglish = "How many letters are in the Arabic language?",
+            questionType = "multipleChoice",
+            audioWord = "",
+            options = new List<string> { "28", "27", "26", "25" },
+            correctAnswer = new List<string> { "28" }
+        },
+        new Exercise
+        {
+            questionArabic = "ما هي أحرف هذه الكلمة؟",
+            questionEnglish = "Which of these letters compose this word?",
+            questionType = "allThatApply",
+            audioWord = "مَلِكْ",
+            options = new List<string> { "الميم", "الكاف", "الألف", "اللام" },
+            correctAnswer = new List<string> { "الميم", "الكاف", "اللام" }
         }
+    },
+    imageLink = "https://assets.rbl.ms/19152954/origin.jpg"
+};
+            return Ok(lesson);
+    }
 
         // Get a lesson by ID
         [HttpGet("{id}")]
